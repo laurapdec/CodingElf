06008ef61fb54a318d8551cbfbef88e096d53285
Laura Mac <laurapdec@hotmail.com>
2022-06-29 06:41:48 +0200

    css changing stuff and scss to css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout } from '../components';
-import '../styles/globals.scss';
+import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
