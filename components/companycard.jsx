644b732ce817271df90772b110434428f8bcea36
Laura Mac <laurapdec@hotmail.com>
2022-07-03 02:24:53 +0200

    Update CompanyCard

--- components/companycard.jsx
+++ components/companycard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function CompanyCard() {
   return (
-    <div className="bg-[#fffae2] shadow-lg rounded-lg p-8 pb-12 mb-8 mx-auto w-full mx-auto  md:w-5/6 lg:mx-0  md:w-2/3 w-full lg:w-full ">
+    <div className="bg-[#fffae2] shadow-lg rounded-lg p-8 pb-12 mb-8 w-full mx-auto  md:w-5/6  lg:w-full lg:mx-0 lg:p-8">
       <h1 className="text-lg font-bold text-center mb-4">About Us</h1>
       <div className="text-justify">
-        <div className="px-2 md:px-8 "> &nbsp; Hi, did you got here after spending hours searching for a solution for your problem? Do you find yourself overwhelmed by some forums answers?<br /><br />
+        <div className="text-sm md:text-base lg:text-lg px-2 md:px-5 "> &nbsp; Hi, did you got here after spending hours searching for a solution for your problem? Do you find yourself overwhelmed by some forums answers?<br /><br />
 
         &nbsp;Don&apos;t worry, it&apos;s completely normal and a healthy sign that you&apos;re growing and becoming a better programmer. 
 Though it&apos;s a gratifying journey, not all suffering is justified, sometimes we just want an easy way to find a solution for a bug or a complete understandable explanation on how to make a certain program.<br /><br />
