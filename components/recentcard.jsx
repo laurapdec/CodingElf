03addc4a0b31bbbe17a4b5c2aa63995d8daaa9a3
Laura Mac <laurapdec@hotmail.com>
2022-07-03 11:39:31 +0200

    Small changes in caroseul
    Padding between caroseul and photos the same in all devices
    Changing button

--- components/recentcard.jsx
+++ components/recentcard.jsx
@@ -6,11 +6,11 @@ import 'react-multi-carousel/lib/styles.css';
 
 const responsive = {
     desktop: {
-      breakpoint: { max: 4000, min: 1000 },
+      breakpoint: { max: 4000, min: 1020 },
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 1000, min: 640 },
+      breakpoint: { max: 1020, min: 640 },
       items: 2,
     },
     mobile: {
@@ -24,7 +24,7 @@ function RecentCard({ posts }) {
   
 
   const customLeftArrow = (
-    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-[#a081a4] rounded-full">
+    <div className=" absolute shadow-xl flex arrow-btn left-0 justify-center py-3 cursor-pointer bg-[#a081a4] rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
@@ -32,7 +32,7 @@ function RecentCard({ posts }) {
   );
 
   const customRightArrow = (
-    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-[#a081a4] rounded-full">
+    <div className="absolute shadow-xl flex arrow-btn right-0 justify-center py-3 cursor-pointer bg-[#a081a4] rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
       </svg>
@@ -40,8 +40,8 @@ function RecentCard({ posts }) {
   );
 
     return (
-    <div className=" shadow-lg bg-black/10  rounded-lg p-8 pb-12 mb-8 mx-auto w-full mx-auto  md:mx-0 md:w-full lg:mx-0   w-full lg:w-full ">
-        <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
+    <div className=" shadow-lg bg-black/10  rounded-lg px-8 py-8 mb-8 mx-auto w-full  sm:px-4 lg:px-2 ">
+        <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass=" sm:px-4 lg:px-6">
             {posts.map((post) => (
                 <PostSquare post={post.node} key={post.title} />
             ))}
