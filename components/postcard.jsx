cca1b8bb0fc6eaee22bb245859f70d0b8ec46687
Laura Mac <laurapdec@hotmail.com>
2022-07-04 00:23:58 +0200

    Like working
    Logo as hyperlink
    Added slug as Likes argument
    Added like functionality
    RecentPosts orderBy created

--- components/postcard.jsx
+++ components/postcard.jsx
@@ -48,7 +48,7 @@ const PostCard = ({ post , tagon="" }) => {
           </svg>
           <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
         </div>
-        <Likes likes={post.likes}  generalstyle={"ml-8"}/>
+        <Likes likesinit={post.likes}  generalstyle={"ml-8"} slug={post.slug}/>
         
       </div>
       <div className="mb-8">
--- services/index.js
+++ services/index.js
@@ -4,103 +4,101 @@ const graphqlAPI = process.env.NEXT_PUB_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-  query MyQuery {
-    postsConnection {
-      edges {
-        node {
-          createdAt
-          title
-          likes
-          slug
-          excerpt {
-            html
-          }
-          content {
-            html
-          }
-          author {
-            name
-            surname
-            picture {
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            createdAt
+            title
+            likes
+            slug
+            excerpt {
+              html
+            }
+            content {
+              html
+            }
+            author {
+              name
+              surname
+              picture {
+                url
+              }
+            }
+            coverImage {
               url
             }
-          }
-          coverImage {
-            url
-          }
-          tag {
-            title
-            slug
+            tag {
+              title
+              slug
+            }
           }
         }
       }
     }
-  }`;
-
+  `;
 
   const result = await request(graphqlAPI, query);
 
   return result.postsConnection.edges;
 };
-
 
 export const getRecentPosts = async () => {
   const query = gql`
-  query MyQuery {
-    postsConnection (orderBy: publishedAt_DESC) {
-      edges {
-        node {
-          createdAt
-          title
-          likes
-          slug
-          excerpt {
-            html
-          }
-          content {
-            html
-          }
-          author {
-            name
-            surname
-            picture {
+    query MyQuery {
+      postsConnection(orderBy: createdAt_DESC) {
+        edges {
+          node {
+            createdAt
+            title
+            likes
+            slug
+            excerpt {
+              html
+            }
+            content {
+              html
+            }
+            author {
+              name
+              surname
+              picture {
+                url
+              }
+            }
+            coverImage {
               url
             }
-          }
-          coverImage {
-            url
-          }
-          tag {
-            title
-            slug
+            tag {
+              title
+              slug
+            }
           }
         }
       }
     }
-  }`;
-
+  `;
 
   const result = await request(graphqlAPI, query);
 
   return result.postsConnection.edges;
 };
 
-export const getAuthors = async() => {
+export const getAuthors = async () => {
   const query = gql`
-  query MyQuery {
-    authorsConnection {
-      edges {
-        node {
-          name
-          biography
-          picture {
-            url
+    query MyQuery {
+      authorsConnection {
+        edges {
+          node {
+            name
+            biography
+            picture {
+              url
+            }
           }
         }
       }
     }
-  }
-  
   `;
 
   const result = await request(graphqlAPI, query);
@@ -108,15 +106,14 @@ export const getAuthors = async() => {
   return result.authorsConnection.edges;
 };
 
-
-export const getTags = async() => {
+export const getTags = async () => {
   const query = gql`
-  query MyQuery {
-    tags {
-          title
-          slug
+    query MyQuery {
+      tags {
+        title
+        slug
+      }
     }
-  }
   `;
 
   const result = await request(graphqlAPI, query);
@@ -124,85 +121,97 @@ export const getTags = async() => {
   return result.tags;
 };
 
-
-
-export const getPostData = async(slug) => {
+export const getPostData = async (slug) => {
   const query = gql`
-  query GetPostData ($slug:String!){
-    post(where:{slug:$slug}){
-      createdAt
-      title
-      slug
-      likes
-      excerpt {
-        html
-      }
-      content {
-        html
-      }
-      author {
-        name
-        surname
-        biography  
-        picture {
-          url
-        }
-      }
-      coverImage {
-        url
-      }
-      tag {
+    query GetPostData($slug: String!) {
+      post(where: { slug: $slug }) {
+        createdAt
         title
         slug
+        likes
+        excerpt {
+          html
+        }
+        content {
+          html
+        }
+        author {
+          name
+          surname
+          biography
+          picture {
+            url
+          }
+        }
+        coverImage {
+          url
+        }
+        tag {
+          title
+          slug
+        }
       }
     }
-  }
   `;
 
-
-  const result = await request(graphqlAPI, query,{slug});
+  const result = await request(graphqlAPI, query, { slug });
 
   return result.post;
 };
 
-
 export const getPostsFromTag = async (tagslug) => {
   const query = gql`
-  query GetPostsFromTag ($tagslug:String!) {
-    postsConnection (where: {tag_some: {slug: $tagslug}}) {
-      edges {
-        node {
-          createdAt
-          title
-          likes
-          slug
-          excerpt {
-            html
-          }
-          content {
-            html
-          }
-          author {
-            name
-            surname
-            picture {
+    query GetPostsFromTag($tagslug: String!) {
+      postsConnection(where: { tag_some: { slug: $tagslug } }) {
+        edges {
+          node {
+            createdAt
+            title
+            likes
+            slug
+            excerpt {
+              html
+            }
+            content {
+              html
+            }
+            author {
+              name
+              surname
+              picture {
+                url
+              }
+            }
+            coverImage {
               url
             }
-          }
-          coverImage {
-            url
-          }
-          tag {
-            title
-            slug
+            tag {
+              title
+              slug
+            }
           }
         }
       }
     }
-  }`;
+  `;
 
-
-  const result = await request(graphqlAPI, query,{tagslug});
+  const result = await request(graphqlAPI, query, { tagslug });
 
   return result.postsConnection.edges;
 };
+
+export const addLike = async ({slug}) => {
+  console.log(slug)
+  if (slug===undefined){
+    return "#"
+  }
+
+  var qtd = getPostData({slug}).likes + 1;
+
+  
+  const result = await request(graphqlAPI,mutation,{qtd: qtd,slug:slug});
+
+  return result
+};
+
+
