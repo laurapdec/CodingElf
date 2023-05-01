43064a449869484127e76d54c6f593ecc6ff1572
Laura Mac <laurapdec@hotmail.com>
2023-05-01 12:21:58 +0200

    most of formating except for sublist, img and code snippet

--- lib/ptcomponents.jsx
+++ lib/ptcomponents.jsx
@@ -1,36 +1,66 @@
+import {Refractor} from 'react-refractor'
 
-  
-const BlockComponent = {
+const blockcomponent = {
   // Ex. 1: customizing common block types
-  h1: ({children}) => <h1 className="text-2xl">{children}</h1>,
-  blockquote: ({children}) => <blockquote className="border-l-purple-500">{children}</blockquote>,
-
-  // Ex. 2: rendering custom styles
-  customHeading: ({children}) => ( <h2 className="text-lg text-primary text-purple-700">{children}</h2> ),
+  h1: ({children}) => <h1 className="text-3xl font-bold">{children}</h1>,
+  h2: ({children}) => <h2 className="text-2xl font-bold">{children}</h2>,
+  h3: ({children}) => <h3 className="text-xl font-bold">{children}</h3>,
+  h4: ({children}) => <h4 className="text-lg font-bold">{children}</h4>,
+  blockquote: ({children}) => <blockquote className="border-l-2 italic  pl-2 border-gray-400">{children}</blockquote>,
+  
   
   // Ex. 2: rendering custom styles
-  normal: ({ children }) => {
-
-    // render the modified text
-    return  <p className="text-gray-50">{'\t' + children}</p>;
-  },
+  normal: ({children}) => (
+    <p className="text-lg text-primary">
+      <span className="ml-4">{children[0]}</span>
+      {children.slice(1)}
+    </p>
+  ),
 }
 
 const ptComponents = {
-  types: {
-    normal: ({ children }) => { return  <p className="text-gray-50">{'\t' + children}</p>},
-    blockquote: ({ children }) => { return  <blockquote className="border-l-purple-500">{children}</blockquote>},
+types: {
     image: ({ value }) => {
-    if (!value?.asset?._ref) {
-        return null
-    }
+        if (!value?.asset?._ref) {
+            return null
+        }
+        return (
+            <img
+            alt={value.alt || ' '}
+            loading="lazy"
+            src={urlFor(value).width(320).height(240).fit('max').auto('format')}
+            />
+        )
+        }
+},
+
+list: {
+  // Ex. 1: customizing common list types
+  bullet: ({children}) => {return(<ul className=" list-disc list-inside">{children}</ul>)},
+  number: ({children}) => {return(<ol className="list-decimal list-inside">{children}</ol>)},
+
+  // Ex. 2: rendering custom lists
+  checkmarks: ({children}) => <ol className="m-auto text-lg">{children}</ol>,
+},
+
+block: blockcomponent,
+code:({props}) => {
+  <Refractor 
+    language={props.value.language}
+    value={props.value.code}
+    markers={props.value.highlightedLines}
+  />
+},
+marks: {
+  link: ({children, value}) => {
+    const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
     return (
-        <img
-        alt={value.alt || ' '}
-        loading="lazy"
-        src={urlFor(value).width(320).height(240).fit('max').auto('format')}
-        />
+      <a href={value.href} rel={rel} className="text-[#5b858f] hover:text-gray-300 font-bold"> 
+        {children}
+      </a>
     )
-    },
-  }
+  },
+},
 }
+
+export default ptComponents
