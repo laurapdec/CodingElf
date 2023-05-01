fc4d12ecdc6fb67c9f817c64fe4fa9af9a532b42
Laura Mac <laurapdec@hotmail.com>
2023-05-01 21:35:57 +0200

    Mais alguns detalhes do rich text

--- lib/ptcomponents.jsx
+++ lib/ptcomponents.jsx
@@ -6,15 +6,15 @@ import { MdContentCopy } from 'react-icons/md';
 
 const blockcomponent = {
   // Ex. 1: customizing common block types
-  h1: ({children}) => <h1 className="text-3xl font-bold">{children}</h1>,
-  h2: ({children}) => <h2 className="text-2xl font-bold">{children}</h2>,
-  h3: ({children}) => <h3 className="text-xl font-bold">{children}</h3>,
-  h4: ({children}) => <h4 className="text-lg font-bold">{children}</h4>,
+  h1: ({children}) => <h1 className="text-3xl mt-6 mb-4 font-bold">{children}</h1>,
+  h2: ({children}) => <h2 className="text-2xl mt-4 mb-2 font-bold">{children}</h2>,
+  h3: ({children}) => <h3 className="text-xl mt-2 mb-1 font-bold">{children}</h3>,
+  h4: ({children}) => <h4 className="text-lg mt-1 mb-1 font-bold">{children}</h4>,
   blockquote: ({children}) => <blockquote className="border-l-2 italic  pl-2 border-gray-400">{children}</blockquote>,
   
   // Ex. 2: rendering custom styles
   normal: ({children}) => (
-    <p className="text-lg text-primary">
+    <p className="text-lg text-primary mt-2">
       <span className="ml-4">{children[0]}</span>
       {children.slice(1)}
     </p>
@@ -30,8 +30,8 @@ const ptComponents = {
       if (!node) return null;
       
       return (
-        <div className=' relative flex flex-wrap'>
-          <SyntaxHighlighter language={node.value.code.language} style={coldarkDark} showLineNumbers='true' className='h-full w-full z-10  rounded-lg'>
+        <div className=' relative flex flex-wrap mt-4 mb-10'>
+          <SyntaxHighlighter language={node.value.code.language} style={coldarkDark} showLineNumbers='true' className='h-full w-full z-10 rounded-lg'>
             {node.value.code.code}
           </SyntaxHighlighter>
           <button className='absolute p-2 rounded-lg right-0 mr-4 mt-6 z-20 bg-gray-600 text-white ' onClick={() => copy(node.value.code.code)}>
